b36334a8f0ae6e5b5a7414681638092f8edb3abd
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-07-22 14:10:24 -0500

    IB Basics

--- HWS/Storm Viewer/Storm Viewer/ViewController.swift
+++ HWS/Storm Viewer/Storm Viewer/ViewController.swift
@@ -12,5 +12,17 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        print(pictures)
     }
 }
---
+++ IBBasics/IBBasics/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  IBBasics
+//
+//  Created by Paul Thomas on 7/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    @IBOutlet var myButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
